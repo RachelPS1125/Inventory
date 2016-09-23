20fe27a614dee00adbb948e39e033e0b3b1af778
Rachel Prehodka-Spindel <rachelps1125@gmail.com>
2016-09-23 11:59:02 -0400

    worked on function to fill borrowers

--- js/script.js
+++ js/script.js
@@ -4,6 +4,22 @@ $(document).ready(function(){
 		$('.open').show();
 		$('.close').hide();
 	}
+	function fillBorrowers(){
+		$.ajax({
+			url: 'https://usdangameinventory-b5d8.restdb.io/rest/borrower-information',
+			method: 'GET',
+			headers: {
+				'x-apikey': '57d1c8248dfe9ef744ec9bfe'
+			}
+		}).done(function(borrowerInfo){
+			console.log(borrowerInfo);
+			for(var i=0; i < borrowerInfo.length; i++){
+				if(games[i].Quantity > 0 && borrowerInfo.active===true){	
+					$('.games-borrowed').append('<div class="borrower-info columns clearfix"><div>'+borrowerInfo.firstName+'</div><div>'+borrowerInfo.lastName+'</div><div>'+borrowerInfo.phoneNumber+'</div><div>+'+borrowerInfo.studentID+'</div><div>'+borrowerInfo.item+'</div><div>Pressme</div><div>Pressm2</div><div>');
+				};	
+			};
+		});
+	fillBorrowers;
 	$('.open').click(function(){
 		$('.side-nav').css('margin-left','0');
 		$('.open').hide();
@@ -106,4 +122,4 @@ $(document).ready(function(){
 			}
 		}).done();
 	});
-});
+};
