a007e991d42b954178dcaec9fa24176690ba738c
Rachel Prehodka-Spindel <RachelPS1125@gmail.com>
2016-09-30 11:10:11 -0400

    some progress

--- js/script.js
+++ js/script.js
@@ -12,14 +12,112 @@ $(document).ready(function(){
 				'x-apikey': '57d1c8248dfe9ef744ec9bfe'
 			}
 		}).done(function(borrowerInfo){
-			console.log(borrowerInfo);
-			for(var i=0; i < borrowerInfo.length; i++){
-				if(games[i].Quantity > 0 && borrowerInfo.active===true){	
-					$('.games-borrowed').append('<div class="borrower-info columns clearfix"><div>'+borrowerInfo.firstName+'</div><div>'+borrowerInfo.lastName+'</div><div>'+borrowerInfo.phoneNumber+'</div><div>+'+borrowerInfo.studentID+'</div><div>'+borrowerInfo.item+'</div><div>Pressme</div><div>Pressm2</div><div>');
+			while(i < borrowerInfo.length){
+				var borrower = borrowerInfo[i];
+				if(borrower.active===true){	
+					var template = $('.template>.games-borrowed').clone();
+					template.attr('data-id', borrower._id);
+					template.find('.first').text(borrower.firstName);
+					template.find('.last').text(borrower.lastName);
+					template.find('.time').text(borrower.timeBorrowed)
+					template.find('.item').text(borrower.itemBorrowed);
+					$('.data-rows').append(template);
 				};	
+				i++
 			};
 		});
-	fillBorrowers;
+	};
+	function fillAV(){
+		$.ajax({
+			url: 'https://usdangameinventory-b5d8.restdb.io/rest/avinventory',
+			method: 'GET',
+			headers: {
+				'x-apikey': '57d1c8248dfe9ef744ec9bfe'
+			}
+		}).done(function(avInventory){
+			while(i < avInventory.length){
+				var avItem = avInventory[i];	
+				var template = $('.template>.games-borrowed').clone();
+				template.find('.first').text(borrower.firstName);
+				template.find('.last').text(borrower.lastName);
+				template.find('.time').text(borrower.timeBorrowed)
+				template.find('.item').text(borrower.itemBorrowed);
+				$('.data-rows').append(template);
+				i++
+			};
+		});
+	}
+	function getInventoryList(table){
+		$.ajax({
+		url: 'https://usdangameinventory-b5d8.restdb.io/rest/' + table,
+		method: 'GET',
+		headers: {
+			'x-apikey': '57d1c8248dfe9ef744ec9bfe'
+		}
+		}).done(function(inventory){
+			$('.inventory-list').empty();
+			for(var i=0; i < inventory.length; i++){
+				if(inventory[i].Quantity > 0){
+					$('.inventory-list').append('<option value="'+inventory[i].Name+'">'+ inventory[i].Name +' - ' + inventory[i].Quantity +'</option>');
+				};
+			};
+		});
+	}
+	function getDate(){
+		var date = new Date();
+		return (date.getMonth()+1) + '/' + date.getDate() + '/' + (date.getYear()-100) + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+	}
+	function updateQuantity(borrowOrReturn, table, that){
+		var borrowID = $(that).parent().attr('data-id');
+		console.log(borrowID);
+		if (borrowOrReturn === 'borrow'){
+			var status = true;
+			var change = -1;
+		}
+		else{
+			var status = false;
+			var change = 1;
+		};
+		$.ajax({
+			url: 'https://usdangameinventory-b5d8.restdb.io/rest/borrower-information/'+borrowID,
+			method: 'PUT',
+			headers: {
+				'x-apikey': '57d1c8248dfe9ef744ec9bfe'
+			},
+			data:{
+				active: status,
+			}
+		}).done(function(){
+			$.ajax({
+				url: 'https://usdangameinventory-b5d8.restdb.io/rest/' +table,
+				method: 'GET',
+				headers: {
+					'x-apikey': '57d1c8248dfe9ef744ec9bfe'
+				},
+				data: {
+					q:JSON.stringify({Name: $(that).parent().find('.item').text()})
+				}
+			}).done(function(item){
+				var itemID = item[0]._id;
+				var itemQuantity = item[0].Quantity;
+				console.log(itemID);
+				console.log(item);
+				$.ajax({
+					url: 'https://usdangameinventory-b5d8.restdb.io/rest/'+ table+'/'+itemID,
+					method: 'PUT',
+					headers: {
+						'x-apikey': '57d1c8248dfe9ef744ec9bfe'
+					},
+					data:{
+						'Quantity': itemQuantity + change,
+					}	
+				}).done(function(){
+				});
+			});
+		});
+	}
+	var i = 0;
+	fillBorrowers();
 	$('.open').click(function(){
 		$('.side-nav').css('margin-left','0');
 		$('.open').hide();
@@ -80,7 +178,7 @@ $(document).ready(function(){
 		$('.av-new').hide();
 		$('.game-new').hide();
 	});
-	$.ajax({
+	/*$.ajax({
 		url: 'https://usdangameinventory-b5d8.restdb.io/rest/games',
 		method: 'GET',
 		headers: {
@@ -88,17 +186,28 @@ $(document).ready(function(){
 		}
 	}).done(function(games){
 		for(var i=0; i < games.length; i++){
-			if(games[i].Quantity > 0){	
-				$('.item-list').append('<option value="'+games[i]._id+'">'+ games[i].Name +' - ' + games[i].Quantity +'</option>');
+			if(games[i].Quantity > 0){
+				$('.game-list').append('<option value="'+games[i].Name+'">'+ games[i].Name +' - ' + games[i].Quantity +'</option>');
 			};
 		};
+	});*/
+	$('.data-rows').on('click', '.returned', function(){
+		var that = this;
+		updateQuantity('return', 'games', that);
+		$(that).parent().remove();
 	});
 	$('.add').click(function(){
-		$('.overlay').fadeIn(1000);
+		$('.overlay').fadeIn(500);
 	});
 	$(".close-window").click(function(event){
 		event.preventDefault();
-		$('.overlay').fadeOut(1000);
+		$('.overlay').fadeOut(100);
+	});
+	$('.game-radio').click(function(){
+		getInventoryList('games');
+	});
+	$('.av-radio').click(function(){
+		getInventoryList('avinventory');
 	});
 	$('.new-borrow').submit(function(event){
 		event.preventDefault();
@@ -106,8 +215,7 @@ $(document).ready(function(){
 		var last = $('.last-name').val();
 		var phone = $('.phone').val();
 		var studentID = $('.id-num').val();
-		var time = $('.time').val();
-		console.log(first);
+		var time = getDate();
 		$.ajax({
 			url: 'https://usdangameinventory-b5d8.restdb.io/rest/borrower-information',
 			method: 'POST',
@@ -116,6 +224,7 @@ $(document).ready(function(){
 				'lastName': last,
 				'phoneNumber': phone,
 				'studentID': studentID,
+				'itemBorrowed': $('.inventory-list').val(),
 				'timeBorrowed': time,
 				'active': true
 			},
@@ -123,7 +232,8 @@ $(document).ready(function(){
 				'x-apikey': '57d1c8248dfe9ef744ec9bfe'
 			}
 		}).done(function(){
-			console.log('record added');
+			fillBorrowers();
+			$('.overlay').fadeOut(100);
 		});
 	});
-};
+});
